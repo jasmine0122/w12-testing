a2ce06767f5b210acf538e205fad81d2a83d1fdc
jasmine0122 <410621223@gms.ndhu.edu.tw>
2022-05-16 22:33:56 +0800

    test first

---
+++ apps/bowling/src/app/game.ts
@@ -0,0 +1,12 @@
+export class Game {
+    basket: any;
+    checkout(): any {
+	    throw new Error('Method not implemented.');
+    }
+    createBook(number: number): any {
+	    throw new Error('Method not implemented.');
+    }
+    addToBasket(arg0: any) {
+	    throw new Error('Method not implemented.');
+    }
+}
