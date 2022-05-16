f5685569492aa5814c3eb536c275a4b3055923bb
jasmine0122 <410621223@gms.ndhu.edu.tw>
2022-05-16 23:37:07 +0800

    test

--- apps/bowling/src/app/game.ts
+++ apps/bowling/src/app/game.ts
@@ -1,8 +1,54 @@
-export class Game {
+export class Potter {
+    price = 8;
+    discounts = [0, 0.05, 0.1, 0.2, 0.25];
     basket: Book[] = [];
 
     checkout(): number {
-        return -1
+        const qtyOfPacksBySize = this.sortBooksInPacks(
+            this.sortBooksByQtyOfRepeatingNumbers()
+        );
+        let total = 0;
+
+        for (let pack = 0; pack < qtyOfPacksBySize.length; pack++) {
+            if (qtyOfPacksBySize[pack] > 0) {
+                const numberOfBooksInPack = pack + 1;
+
+                const applicableDiscount = this.discounts[pack];
+
+                const subtotal = numberOfBooksInPack * this.price;
+
+                const discountedPriceForPack =
+                    subtotal - subtotal * applicableDiscount;
+
+                total += qtyOfPacksBySize[pack] * discountedPriceForPack;
+            }
+        }
+        return total;
+    }
+
+    sortBooksInPacks(booksByQtyOfNumbers: number[]): number[] {
+        const packs: number[] = new Array(this.discounts.length + 1).fill(0);
+
+        for (let qty = 0; qty < booksByQtyOfNumbers.length; qty++) {
+            if (booksByQtyOfNumbers[qty] > 0) {
+                packs[qty] = booksByQtyOfNumbers[qty];
+                booksByQtyOfNumbers = booksByQtyOfNumbers.map((q) => {
+                    return q > 0 ? q - booksByQtyOfNumbers[qty] : q;
+                });
+            }
+        }
+        return packs.reverse();
+    }
+
+    sortBooksByQtyOfRepeatingNumbers(): number[] {
+        const qtyOfBooksByNumber: number[] = new Array(
+            this.discounts.length + 1
+        ).fill(0);
+
+        this.basket.forEach((b) => {
+            qtyOfBooksByNumber[b.number] += 1;
+        });
+        return qtyOfBooksByNumber.sort();
     }
 
     createBook(bookNumber: number): Book {
@@ -10,11 +56,10 @@ export class Game {
     }
 
     addToBasket(book: Book) {
-        this.basket.push(book);   
-
+        this.basket.push(book);
     }
 }
 
 class Book {
-    constructor(protected bookNumber: number) {}
+    constructor(public number: number) {}
 }
